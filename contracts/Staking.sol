2d0ca3b84f03b425b46363ad07af6cd45cc245d5
AyDeveloper <ayoayobami20@gmail.com>
2022-03-26 17:37:05 -0700

    new changes

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -43,21 +43,23 @@ contract Staking is ERC20 {
     }
 
     function stake(uint _amount) public returns(uint _total, uint _new) {
+        uint amountNow = _amount / 1e18;
         // set an id that numbers count;
         // require(boredApe.balanceOf(msg.sender) > 1 , "Staking restricted to boredApe Owners");
         // transfer from msg.sender to address this
         // for withdraw, transfer from address this to msg.sender
         // require the stake token balance of msg.sender is greater than amount
 
-        require(_amount > 0, "Cannot stake less than zero");
+        require(amountNow > 0, "Cannot stake less than zero");
         Staker storage stakerObject = stakers[msg.sender];
          
         if(stakerObject.staked) {
-            stakeBalances[msg.sender] += _amount;
-            transfer(address(this), _amount);
-            stakerObject.amountStaked += _amount;
+            stakeBalances[msg.sender] += amountNow;
+            transfer(address(this), amountNow);
+            stakerObject.amountStaked += amountNow;
             stakerObject.lastStakedTime = block.timestamp;
-            (,, uint reward) = yield(stakerObject.compoundStake, stakerObject.startTime);
+            // (uint _time, uint reward, uint _newOne) = calculateYield(stakerObject.compoundStake);
+            (,, uint reward) = yield(stakerObject.compoundStake, stakerObject.lastStakedTime);
               _total = stakerObject.compoundStake + reward;
             (,,_new) = calculateReyield(_total, stakerObject.startTime);
             stakerObject.withdrawalAmount = _new + _total;
@@ -65,7 +67,7 @@ contract Staking is ERC20 {
             // do a mapping  that compound amount each time they stake and calculate the yield
             // compunding works with previous total(amount + interest ) + new amount;
         }
-            uint newStake = _total + _amount;
+            uint newStake = _total + amountNow;
             stakeBalances[msg.sender] = newStake;
             transfer(address(this), newStake);
             stakerObject.amountStaked = newStake;
@@ -86,9 +88,9 @@ contract Staking is ERC20 {
             _transfer(address(this), msg.sender, stakerObject.amountStaked);
             // _balances[address(this)] -= stakerObject.amountStaked;
 
-        } else {
-            emit  withdrawEvent(stakerObject.withdrawalAmount,msg.sender);
-                transfer(msg.sender, stakerObject.withdrawalAmount);
+        } else {            
+                _transfer(address(this), msg.sender, stakerObject.withdrawalAmount);
+                 emit  withdrawEvent(stakerObject.withdrawalAmount,msg.sender);
 
                     stakerObject.amountStaked = 0;
                     stakerObject.staked = false;
@@ -108,13 +110,13 @@ contract Staking is ERC20 {
        return   ((_amount * 10) / 100) * 10000;
     }
 
-    function _useInterest(uint _amount) internal pure returns (uint) {
+    function _useInterest(uint _amount) public pure returns (uint) {
         uint interest = (_getInterestPersec(_amount) / 10000000);
         return interest;
     }
 
      function calculateYield(uint _amount) public view  returns(uint _a, uint _b, uint _c) {
-         if(block.timestamp - stakers[msg.sender].startTime > 30) {
+         if(block.timestamp - stakers[msg.sender].startTime > 300 seconds) {
             _a = uint(block.timestamp - stakers[msg.sender].startTime);
                 _b = _useInterest(_amount);
                 _c =  _a * _b;
--- test/index.ts
+++ test/index.ts
@@ -12,7 +12,7 @@ describe("Staking Contract", function () {
     const signer1 = await ethers.getSigner(signer.address);
 
     const GhostContractAddr = await ethers.getContractFactory("Staking");
-    ghostContractAddr = await GhostContractAddr.connect(signer1).deploy("GHoST", "Ghst", "100000000000000000000000", signer.address);
+    ghostContractAddr = await GhostContractAddr.connect(signer1).deploy("GHoST", "Ghst", "100000000000000000000000000000000", signer.address);
     ghostContractAddr.deployed();
   });
 
@@ -30,7 +30,7 @@ describe("Staking Contract", function () {
     const stake = await ghostContractAddr.connect(signer1).stake("100000000000000000000");
     const balContract = await ghostContractAddr.balanceOf(ghostContractAddr.address);
     console.log(`This contract addr now has ${balContract}`);
-    expect(balContract.toString()).to.equal("100000000000000000000");
+    expect(balContract.toString()).to.equal("100");
   });
   it("Withdraw stake b4 30 secs", async function () {
     const [,signer] = await ethers.getSigners()
@@ -51,8 +51,70 @@ describe("Staking Contract", function () {
     console.log(`user balance now is ${userBal}`);
     console.log(`contract balance after withdraw ${contractBal}`);
     expect(balContract.toString()).to.equal("0");
-    expect("100000000000000000000000").to.equal( userBal.toString());
+    expect("100000000000000000000000000000000").to.equal( userBal.toString());
+  });
+  it("Withdraw stake after jumping time", async function () {
+    const [,signer] = await ethers.getSigners()
+    const signer1 = await ethers.getSigner(signer.address);
+    const balContract = await ghostContractAddr.balanceOf(ghostContractAddr.address);
+    console.log(`contract bal b4 stake ${balContract}`); 
+    const userBal1 = await ghostContractAddr.balanceOf(signer.address);  
+    console.log(`user bal before ${userBal1}`);
+  //   const stake = await ghostContractAddr.connect(signer1).stake("10000000000000000000000000000");
+  //   const balContract1 = await ghostContractAddr.balanceOf(ghostContractAddr.address);
+  //   const userBalAfterStake = await ghostContractAddr.balanceOf(signer.address);
+  //   console.log(`user balance after stake ${userBalAfterStake}`);
+  //   console.log(`This contract addr now has ${balContract1}`);
+  //   // jump time
+  // console.log(`time successfully skipped`);
+  // // console.log(await ghostContractAddr._useInterest("10000000000000000000000000000"));
+  // // console.log(await ghostContractAddr.calculateYield("10000000000000000000000000000"));
+  
+  // const transferToContract = await ghostContractAddr.connect(signer1).transfer(ghostContractAddr.address, "7000000000000000000000000000000");
+  // const balContract2 = await ghostContractAddr.balanceOf(ghostContractAddr.address);
+  // console.log(`This contract addr bal after jump time ${balContract2}`);
+
+  //   // @ts-ignore
+  //   await network.provider.send("evm_increaseTime", [259200]);
+  //   // @ts-ignore
+  //   await network.provider.send("evm_mine");
+
+  //   const withdrawNow = await ghostContractAddr.connect(signer1).withdrawStake();
+  //     await withdrawNow.wait();
+
+  //   const contractBal = await ghostContractAddr.balanceOf(ghostContractAddr.address);
+  //   const userBal = await ghostContractAddr.balanceOf(signer.address);
+  //   console.log(`user balance now is ${userBal}`);
+  //   console.log(`contract balance after withdraw ${contractBal}`);
+  });
+  it("return stake details after jumping time", async function () {
+    const [,signer] = await ethers.getSigners()
+    const signer1 = await ethers.getSigner(signer.address);
+    const balContract = await ghostContractAddr.balanceOf(ghostContractAddr.address);
+    console.log(`contract bal b4 stake ${balContract}`); 
+    const userBal1 = await ghostContractAddr.balanceOf(signer.address);  
+    console.log(`user bal before ${userBal1}`);
+    
+    const stake = await ghostContractAddr.connect(signer1).stake("10000000000000000000000000000");
+    await stake.wait();
+
+     // @ts-ignore
+     await network.provider.send("evm_increaseTime", [259200]);
+     // @ts-ignore
+     await network.provider.send("evm_mine");
+    // for (let i = 0; i < 2; i++) {
+    // }
+
+    const stake2 = await ghostContractAddr.connect(signer1).stake("10000000000000000000000000000");
+    await stake2.wait();
+
+    // @ts-ignore
+    await network.provider.send("evm_increaseTime", [259200]);
+    // @ts-ignore
+    await network.provider.send("evm_mine");
+
+    const getUserStake = await ghostContractAddr.checkStake();
+    console.log(getUserStake);
   });
 
-  
 });
